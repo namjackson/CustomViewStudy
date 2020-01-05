0a14fd1c3fd1f4c4964db0766ba2905aef5bcf21
namjackson <skawo32167@naver.com>
2020-01-05 21:44:53 +0900

    codelab 2-1.Drawing on Canvas Object

--- MiniPaint/app/src/main/java/com/namjackson/minipaint/MainActivity.kt
+++ MiniPaint/app/src/main/java/com/namjackson/minipaint/MainActivity.kt
@@ -2,11 +2,19 @@ package com.namjackson.minipaint
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val myCanvasView = MyCanvasView(this)
+        myCanvasView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
+
+        setContentView(myCanvasView)
+
+//        setContentView(R.layout.activity_main)
     }
 }
